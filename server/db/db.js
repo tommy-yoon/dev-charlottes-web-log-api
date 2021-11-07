146d6182b7af646499bb07537199f52d9ceeff25
Vimal <46978245+vimal-raj-nagarajan@users.noreply.github.com>
2021-11-08 10:52:07 +1300

    First commit

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,11 @@
 const config = require('./knexfile').development
 const db = require('knex')(config)
 
+function blogPosts (db = db) {
+  return db('posts')
+    .select()
+}
+
 module.exports = {
+  blogPosts
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -5,5 +5,17 @@ const db = require('../db/db')
 const router = express.Router()
 
 // put routes here
+router.get('/', (req, res) => {
+  console.log(req)
+  db.blogPosts()
+    .then(result => {
+      console.log(result)
+      return null
+    })
+    .catch(err => {
+      console.log(err)
+      return null
+    })
+})
 
 module.exports = router
